0f5580cd4c8a6a404a730a1485b39a9d1ef0acb4
Semyon Zernov <sem.z@protonmail.com>
2020-08-08 20:29:19 +0300

    Made implementation with map

--- ex2/urlshort/urlshort.go
+++ ex2/urlshort/urlshort.go
@@ -11,8 +11,16 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if link, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, link, http.StatusMovedPermanently)
+			return
+		}
+
+		fallback.ServeHTTP(w, r)
+	})
 }
 
 // YAMLHandler will parse the provided YAML and then return
