f61cb3c7d3a589d178625ead575e837525b1620b
Semyon Zernov <sem.z@protonmail.com>
2020-10-21 22:08:51 +0300

    Refactored code after watching solution

---
+++ ex5/sitemap/doc.go
@@ -0,0 +1,4 @@
+// sitemap package builds a map of all of the pages within
+// a specific domain and provides it in specific xml format -
+// https://www.sitemaps.org/schemas/sitemap/0.9/
+package sitemap
--- ex5/sitemap/sitemap.go
+++ ex5/sitemap/sitemap.go
@@ -1,114 +1,51 @@
-// sitemap package builds a map of all of the pages within
-// a specific domain and provides it in specific xml format -
-// https://www.sitemaps.org/schemas/sitemap/0.9/
 package sitemap
 
 import (
 	"encoding/xml"
-	"flag"
-	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"strings"
 
 	link "github.com/semka95/gophercises/ex4"
 )
 
-const Xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
 
-// Urlset represents sitemap xml struct https://www.sitemaps.org/schemas/sitemap/0.9/
-type Urlset struct {
-	XMLName xml.Name `xml:"urlset"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Links   []Loc    `xml:"url"`
-}
-
-// Loc represents loc field in sitemap xml struct
-type Loc struct {
-	URL string `xml:"loc"`
-}
-
-// CLI runs the go-sitemap command line app and returns its exit status.
-func CLI(args []string) int {
-	var app appEnv
-	err := app.fromArgs(args)
-	if err != nil {
-		return 2
-	}
-	if err = app.run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Runtime error: %v\n", err)
-		return 1
-	}
-	return 0
-}
-
-// appEnv represents parsed command line arguments
-type appEnv struct {
-	link         string
-	outputFile   string
-	depth        int
+// Sitemap represents data needed to build sitemap using BuildSitemap
+type Sitemap struct {
+	rootLink     string
 	visitedLinks map[string]struct{}
 }
 
-// fromArgs parses command line arguments into appEnv struct
-func (app *appEnv) fromArgs(args []string) error {
-	fl := flag.NewFlagSet("go-sitemap", flag.ContinueOnError)
-
-	l := fl.String(
-		"link", "", "Link to the website to build sitemap",
-	)
-	fl.StringVar(
-		&app.outputFile, "output", "", "Path to output .xml file. By default outputs to stdout",
-	)
-	fl.IntVar(&app.depth, "depth", -1, "Maximum number of links to follow when building a sitemap. By default depth is not set")
-
-	if err := fl.Parse(args); err != nil {
-		return err
+// NewSitemap creates instance of Sitemap
+func NewSitemap(rootLink string) Sitemap {
+	v := map[string]struct{}{
+		rootLink: {},
 	}
 
-	if *l == "" {
-		fmt.Fprintf(os.Stderr, "got bad link: %q\n", *l)
-		fl.Usage()
-		return flag.ErrHelp
+	s := Sitemap{
+		rootLink:     rootLink,
+		visitedLinks: v,
 	}
-	app.link = *l
-
-	app.visitedLinks = make(map[string]struct{})
-	app.visitedLinks[app.link] = struct{}{}
-
-	return nil
+	return s
 }
 
-func (app *appEnv) run() error {
-	queue := []string{app.link}
-	output := os.Stdout
+// urlset represents sitemap xml struct https://www.sitemaps.org/schemas/sitemap/0.9/
+type urlset struct {
+	XMLName xml.Name `xml:"urlset"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Links   []loc    `xml:"url"`
+}
 
-	err := app.BuildSitemap(queue, app.depth)
-	if err != nil {
-		return err
-	}
-
-	if app.outputFile != "" {
-		output, err = os.OpenFile(app.outputFile, os.O_WRONLY|os.O_CREATE, 0755)
-		if err != nil {
-			return err
-		}
-		defer output.Close()
-	}
-
-	err = app.buildXML(output)
-	if err != nil {
-		return err
-	}
-
-	return nil
+// loc represents loc field in sitemap xml struct
+type loc struct {
+	URL string `xml:"loc"`
 }
 
 // BuildSitemap visits links in queue and parses links in other queue and
-// recursively visits them. If depth is greater than zero it defines number
-// of links to follow. All visited links recorded to appEnv.visitedLinks
-func (app *appEnv) BuildSitemap(queue []string, depth int) error {
+// recursively visits them. If depth is greater than zero it restricts number
+// of recursive calls. All visited links recorded to Sitemap.visitedLinks
+func (s *Sitemap) BuildSitemap(queue []string, depth int) error {
 	if depth == 0 {
 		return nil
 	}
@@ -121,7 +58,7 @@ func (app *appEnv) BuildSitemap(queue []string, depth int) error {
 			return err
 		}
 
-		l, err := app.parseLinks(resp.Body)
+		l, err := s.parseLinks(resp.Body)
 		if err != nil {
 			return err
 		}
@@ -132,15 +69,15 @@ func (app *appEnv) BuildSitemap(queue []string, depth int) error {
 
 	if len(discoveredLinks) > 0 {
 		depth--
-		return app.BuildSitemap(discoveredLinks, depth)
+		return s.BuildSitemap(discoveredLinks, depth)
 	}
 
 	return nil
 }
 
-// parseLinks reads from io.Reader and creates array of links.
-// It only parses links with appEnv.link domain
-func (app *appEnv) parseLinks(r io.Reader) ([]string, error) {
+// parseLinks reads html data from io.Reader and creates array of links.
+// It only parses links with Sitemap.rootLink domain
+func (s *Sitemap) parseLinks(r io.Reader) ([]string, error) {
 	res, err := link.ParseHTML(r)
 	if err != nil {
 		return nil, err
@@ -150,46 +87,53 @@ func (app *appEnv) parseLinks(r io.Reader) ([]string, error) {
 
 	for _, v := range res {
 		href := v.Href
-		if strings.HasPrefix(href, app.link) {
-			if _, ok := app.visitedLinks[href]; !ok {
-				app.visitedLinks[href] = struct{}{}
-				links = append(links, href)
-				continue
-			}
+		visited := true
+
+		switch {
+		case strings.HasPrefix(href, s.rootLink):
+			visited = s.isVisited(href)
+		case strings.HasPrefix(href, "/"):
+			href = s.rootLink + href
+			visited = s.isVisited(href)
 		}
 
-		if strings.HasPrefix(href, "/") {
-			href = app.link + href
-			if _, ok := app.visitedLinks[href]; !ok {
-				app.visitedLinks[href] = struct{}{}
-				links = append(links, href)
-			}
+		if !visited {
+			links = append(links, href)
 		}
 	}
 
 	return links, nil
 }
 
+// isVisited checks if url visited
+func (s *Sitemap) isVisited(href string) (visited bool) {
+	if _, visited = s.visitedLinks[href]; !visited {
+		s.visitedLinks[href] = struct{}{}
+	}
+
+	return visited
+}
+
 // buildXML builds xml using appEnv.visitedLinks and writes it
 // to io.Writer
-func (app *appEnv) buildXML(w io.Writer) error {
+func (s *Sitemap) buildXML(w io.Writer) error {
 	_, err := w.Write([]byte(xml.Header))
 	if err != nil {
 		return err
 	}
 
-	links := make([]Loc, 0, len(app.visitedLinks))
-	for k := range app.visitedLinks {
-		links = append(links, Loc{URL: k})
+	links := make([]loc, 0, len(s.visitedLinks))
+	for k := range s.visitedLinks {
+		links = append(links, loc{URL: k})
 	}
 
-	urlset := &Urlset{
-		Xmlns: Xmlns,
+	urlset := &urlset{
+		Xmlns: xmlns,
 		Links: links,
 	}
 
 	enc := xml.NewEncoder(w)
-	enc.Indent("  ", "    ")
+	enc.Indent("", "  ")
 
 	if err := enc.Encode(urlset); err != nil {
 		return err
