34e0ef7859a471644199ab0ff7496c98af9bfb08
Semyon Zernov <sem.z@protonmail.com>
2021-03-07 23:47:16 +0300

    Refactored code after watching solution

--- ex6/caesar.go
+++ ex6/caesar.go
@@ -9,8 +9,6 @@ var alphabet = [26]rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', '
 var alphabetMap = map[rune]int{'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
 
 func caesarCipher(s string, k int) string {
-	k = k % len(alphabet)
-	shifted := append(alphabet[k:], alphabet[:k]...)
 	var b strings.Builder
 	for _, r := range s {
 		i, ok := alphabetMap[unicode.ToLower(r)]
@@ -21,7 +19,7 @@ func caesarCipher(s string, k int) string {
 			}
 			continue
 		}
-		encR := shifted[i]
+		encR := alphabet[(i+k)%26]
 		if unicode.IsUpper(r) {
 			encR = unicode.ToUpper(encR)
 		}
